f5332695fc229046f776781167fa7bb0d1def3f5
rose <vcdaquiado@gmail.com>
2020-11-26 16:42:06 +0100

    code refactored and added now playing bar and left navigation in index file

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+
+	include_once 'index.php';
+?>
+
+	<!--Footer -- NOT LOGGED IN-->
+
+	<footer class="container-fluid" style="background-color: blue;" >
+					<div class="container-fluid">
+						<h5>Buy one take one</h5>
+					</div>
+				</footer>
+
+
+</body>
+</html>
